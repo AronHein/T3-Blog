708520db5a42160c0de309dd697d41c747646248
Aron Hein <heinaron@gmail.com>
2025-01-29 13:25:40 +0200

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,136 @@
+import React from "react";
+import { VscThreeBars } from "react-icons/vsc";
+import { GoBell } from "react-icons/go";
+import { FaRegEdit } from "react-icons/fa";
+import { IoIosSearch } from "react-icons/io";
+import { GoChevronDown } from "react-icons/go";
+
+const HomePage = () => {
+  return (
+    <div className="flex h-screen w-full flex-col">
+      <header className="flex h-20 w-full flex-row items-center justify-around border-b border-gray-200 bg-white">
+        <div className="text-2xl text-gray-600">
+          <VscThreeBars />
+        </div>
+        <div className="text-xl font-thin">Blog</div>
+        <div className="flex items-center space-x-4">
+          <div>
+            <GoBell className="text-2xl text-gray-600" />
+          </div>
+          <div className="h-5 w-5 rounded-full bg-gray-600"></div>
+          <div>
+            <button className="flex  items-center space-x-3 rounded border border-gray-200 px-4 py-2 transition hover:border-gray-900 hover:text-gray-900">
+              <div>Write</div>
+              <div>
+                <FaRegEdit />
+              </div>
+            </button>
+          </div>
+        </div>
+      </header>
+      <section className="grid h-full w-full grid-cols-12 place-items-center">
+        <main className="col-span-8 h-full w-full border-r border-gray-300 px-24">
+          <div className="flex w-full flex-col space-y-4 py-10">
+            <div className="flex w-full items-center space-x-4">
+              <label
+                htmlFor="search"
+                className="relative w-full rounded-3xl border border-gray-900"
+              >
+                <div className="absolute left-2 flex h-full items-center">
+                  <IoIosSearch />
+                </div>
+                <input
+                  type="text"
+                  name="search"
+                  id="search"
+                  className=" w-full rounded-3xl px-4 py-1 pl-7 text-sm outline-none placeholder:text-sm placeholder:text-gray-300"
+                  placeholder="Search..."
+                />
+              </label>
+              <div className="flex w-full items-center justify-end space-x-4">
+                <div>Topics</div>
+                <div className="flex items-center space-x-2">
+                  {Array.from({ length: 4 }).map((_, i) => (
+                    <div
+                      key={i}
+                      className="rounded-3xl bg-gray-200/50 px-4 py-3"
+                    >
+                      tag {i}
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <div className="flex w-full items-center justify-between border-b border-gray-300 pb-8">
+              <div>Articles</div>
+              <div>
+                <button className="flex items-center space-x-2 rounded-3xl border border-gray-900 px-4 py-1.5 font-semibold">
+                  <div>Following</div>
+                  <div>
+                    <GoChevronDown className="text-xl" />
+                  </div>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="flex w-full flex-col justify-center space-y-8">
+            {Array.from({ length: 5 }).map((_, i) => (
+              <div
+                key={i}
+                className="group flex flex-col space-y-4 border-b border-gray-300 pb-8 last:border-none"
+              >
+                <div className="flex w-full items-center space-x-2">
+                  <div className="h-10 w-10 rounded-full bg-gray-300"></div>
+                  <div>
+                    <p className="font-semibold">
+                      Firstname Lastname &#x2022; 29 Jan. 2025
+                    </p>
+                    <p className="text-sm">Some bio text</p>
+                  </div>
+                </div>
+                <div className="grid w-full grid-cols-12 gap-4">
+                  <div className="col-span-8 flex flex-col space-y-4">
+                    <p className="text-2xl font-bold text-gray-800 decoration-indigo-600 group-hover:underline">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Repudiandae, temporibus.
+                    </p>
+                    <p className="break-words text-sm text-gray-500">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Dolorum aut corrupti nemo modi a totam consequuntur ullam
+                      provident delectus et consequatur magnam exercitationem
+                      esse beatae numquam quaerat obcaecati, harum eum ipsa
+                      asperiores. Beatae dolor quasi hic ab dolores omnis modi
+                      odio exercitationem aliquam cumque id dolorem iste, ullam
+                      molestiae voluptatum rem pariatur enim quisquam minima
+                      repellat aperiam. Recusandae, esse error?
+                    </p>
+                  </div>
+                  <div className="col-span-4">
+                    <div className="h-full w-full transform rounded-xl bg-gray-200 transition duration-300 hover:scale-105 hover:shadow-xl"></div>
+                  </div>
+                </div>
+                <div>
+                  <div className="flex w-full items-center justify-start space-x-4">
+                    <div className="flex items-center space-x-2">
+                      {Array.from({ length: 4 }).map((_, i) => (
+                        <div
+                          key={i}
+                          className="rounded-2xl bg-gray-200/50 px-5 py-3"
+                        >
+                          tag {i}
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </main>
+        <aside className="col-span-4 h-full w-full">Sidebar</aside>
+      </section>
+    </div>
+  );
+};
+
+export default HomePage;
