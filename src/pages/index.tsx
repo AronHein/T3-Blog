6bb211462e6ff7a34d2b99f96f948da09480b2cf
Aron Hein <heinaron@gmail.com>
2025-01-29 17:05:54 +0200

    add google authentication

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,11 @@
 import React from "react";
-import { VscThreeBars } from "react-icons/vsc";
-import { GoBell } from "react-icons/go";
-import { FaRegEdit } from "react-icons/fa";
 import { IoIosSearch } from "react-icons/io";
 import { GoChevronDown } from "react-icons/go";
+import MainLayout from "../layouts/MainLayout";
 
 const HomePage = () => {
   return (
-    <div className="flex h-full w-full flex-col">
-      <header className="flex h-20 w-full flex-row items-center justify-around border-b border-gray-200 bg-white">
-        <div className="text-2xl text-gray-600">
-          <VscThreeBars />
-        </div>
-        <div className="text-xl font-thin">Blog</div>
-        <div className="flex items-center space-x-4">
-          <div>
-            <GoBell className="text-2xl text-gray-600" />
-          </div>
-          <div className="h-5 w-5 rounded-full bg-gray-600"></div>
-          <div>
-            <button className="flex  items-center space-x-3 rounded border border-gray-200 px-4 py-2 transition hover:border-gray-900 hover:text-gray-900">
-              <div>Write</div>
-              <div>
-                <FaRegEdit />
-              </div>
-            </button>
-          </div>
-        </div>
-      </header>
+    <MainLayout>
       <section className="grid grid-cols-12">
         <main className="col-span-8 h-full w-full border-r border-gray-300 px-24">
           <div className="flex w-full flex-col space-y-4 py-10">
@@ -177,7 +155,7 @@ const HomePage = () => {
           </div>
         </aside>
       </section>
-    </div>
+    </MainLayout>
   );
 };
 
